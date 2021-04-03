0704fc1e40ab5a363069f64743ca3715a32014b5
homoantropos <homoantropos@gmail.com>
2021-04-03 16:18:29 +0300

    activities and sports main pages ready toDo - both are needed paginator and sort options

--- src/app/main-layout/activities-page/activities-page.component.ts
+++ src/app/main-layout/activities-page/activities-page.component.ts
@@ -1,37 +1,40 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MockDataBase} from '../../thoseWillBeDeletedAfterDBCreating/mockDB';
+import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
-import {Activity} from '../../shared/interfases';
-import {MatPaginator} from '@angular/material/paginator';
 import {Router} from '@angular/router';
-import {MatSort} from '@angular/material/sort';
+import {Observable} from 'rxjs';
+
+import {Activity} from '../../shared/interfases';
+import {ActivityService} from '../../shared/services/activity.service';
 
 @Component({
   selector: 'app-activities-page',
   templateUrl: './activities-page.component.html',
   styleUrls: ['./activities-page.component.css']
 })
-export class ActivitiesPageComponent implements AfterViewInit {
+
+export class ActivitiesPageComponent implements OnInit {
+
   displayedColumns: string[] = ['title', 'author', 'date'];
-  dataSource: MatTableDataSource<Activity> =
-    new MatTableDataSource<Activity>(
-      MockDataBase.mockActivitiesDataBase.filter(a => a.kindOfActivity === 'physical culture')
-    );
+
   // @ts-ignore
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  activities$: Observable<Array<Activity>>;
   // @ts-ignore
-  @ViewChild(MatSort) sort: MatSort;
+  dataSource: MatTableDataSource<Activity>;
+  // @ts-ignore
+  aSub: Subscription;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private activityService: ActivityService
+  ) {
+  }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.activities$ = this.activityService.getAllActivity('physical culture');
   }
 
   goToActivityDetails(a: Activity): void {
     this.router.navigateByUrl(`/activities/${a._id}`);
   }
+
 }
--- src/app/main-layout/sports-page/sports-page.component.ts
+++ src/app/main-layout/sports-page/sports-page.component.ts
@@ -1,43 +1,35 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MockDataBase} from '../../thoseWillBeDeletedAfterDBCreating/mockDB';
+import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
-import {Activity} from '../../shared/interfases';
-import {MatPaginator} from '@angular/material/paginator';
-import {ActivityService} from '../../shared/services/activity.service';
 import {Router} from '@angular/router';
-import {MatSort} from '@angular/material/sort';
+import {Observable} from 'rxjs';
+
+import {Activity} from '../../shared/interfases';
+import {ActivityService} from '../../shared/services/activity.service';
+
 
 @Component({
   selector: 'app-sports-page',
   templateUrl: './sports-page.component.html',
   styleUrls: ['./sports-page.component.css']
 })
-export class SportsPageComponent implements OnInit, AfterViewInit {
 
-  // @ts-ignore
-  sports: Array<Activity>;
-  // @ts-ignore
-  dataSource: MatTableDataSource<Activity>;
-  // @ts-ignore
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  // @ts-ignore
-  @ViewChild(MatSort) sort: MatSort;
+export class SportsPageComponent implements OnInit {
+
   displayedColumns: string[] = ['title', 'author', 'date'];
 
-  constructor(
-    private activityServise: ActivityService,
-    private router: Router
-  ) { }
+  // @ts-ignore
+  sports$: Observable<Array<Activity>>;
+  // @ts-ignore
+  dataSource: MatTableDataSource<Activity>;
 
-  ngOnInit(): void {
-    this.activityServise.getAllActivity('sport')
-      .subscribe(s => this.sports = s);
-    this.dataSource = new MatTableDataSource<Activity>(this.sports);
+  constructor(
+    private activityService: ActivityService,
+    private router: Router
+  ) {
   }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.sports$ = this.activityService.getAllActivity('sport');
   }
 
   goToActivityDetails(a: Activity): void {
