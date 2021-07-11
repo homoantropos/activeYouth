d08cc4a3188700e48d28aed07337024bb801433b
Sergii Antropov <homoantropos@gmail.com>
2021-07-11 16:48:32 +0300

    coaches superadmin dashboard added

---
+++ src/app/shared/components/deletion-confirmer/deletion-confirmer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deletion-confirmer',
+  templateUrl: './deletion-confirmer.component.html',
+  styleUrls: ['./deletion-confirmer.component.css']
+})
+export class DeletionConfirmerComponent implements OnInit {
+
+  show = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
