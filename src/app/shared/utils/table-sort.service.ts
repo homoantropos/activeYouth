a37e2c71b318ae5ebbd67122bf168c1d7d88d604
Sergii Antropov <homoantropos@gmail.com>
2021-07-28 18:14:26 +0300

    sort table service added

---
+++ src/app/shared/utils/table-sort.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableSortService {
+
+  constructor() {
+  }
+
+  sortTableByStringValues(sortOption: any, sortedArray: Array<any>, direction: boolean): boolean {
+    if (typeof sortedArray[0][sortOption] === 'string') {
+      if (direction) {
+        sortedArray.sort((a, b) => b[sortOption].toLowerCase().localeCompare(a[sortOption].toLowerCase()));
+      } else {
+        sortedArray.sort((a, b) => a[sortOption].toLowerCase().localeCompare(b[sortOption].toLowerCase()));
+      }
+    } else {
+      if (direction) {
+        sortedArray.sort((a, b) => b[sortOption] - a[sortOption]);
+      } else {
+        sortedArray.sort((a, b) => a[sortOption] - b[sortOption]);
+      }
+    }
+    return !direction;
+  }
+}
